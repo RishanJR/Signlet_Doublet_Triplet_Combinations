dc4bf82ef3bb223edd5c643e286a537a9a8f0a6d
RishanJR <rishanrajkumar@yahoo.com>
2020-09-27 09:56:49 +0400

    Flip coin to display heads or tails

--- Flip_Coin_Combination.sh
+++ Flip_Coin_Combination.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 echo -e "\nWelcome to Flip Coin Simulation"
-echo -e "This problem displays the winning percentage of head or tail combinatiion in a singlet, doublet and triplet"
+echo -e "This problem displays the winning percentage of head or tail combinatiion in a singlet, doublet and triplet\n"
 
+#Random to get any two numbers and and store is RES
+RES=$(( RANDOM%2 ))
+
+#Checking if RES is 0 or 1
+#0 for heads and 1 for tails
+if [[ $RES -eq 0 ]]
+then
+	echo HEADS
+else
+	echo TAILS
+fi
 
